0cad86b6adafe0372bd856357337356218713f8b
Hansimov <591172499@qq.com>
2024-06-25 14:54:01 +0800

    :gem: [Feature] New SearchBar draft, and remove unused components

---
+++ quasar/src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-input v-model="search" filled type="search" hint="Search">
+    <template v-slot:append>
+      <q-icon name="search" />
+    </template>
+  </q-input>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    return {
+      search: ref(''),
+    };
+  },
+};
+</script>
