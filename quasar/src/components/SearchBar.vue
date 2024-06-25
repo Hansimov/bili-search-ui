611a858f01f00010825b4f294ff360d5042d3929
Hansimov <591172499@qq.com>
2024-06-26 01:55:54 +0800

    :gem: [Feature] SearchBar: get suggestions on input value changes

--- quasar/src/components/SearchBar.vue
+++ quasar/src/components/SearchBar.vue
@@ -4,9 +4,10 @@
     outlined
     clearable
     placeholder="Ask me anything ..."
-    v-model="search"
     type="search"
     style="width: 680px"
+    v-model="search"
+    @update:model-value="debouncedSuggest"
   >
     <template v-slot:prepend>
       <q-btn unelevated padding="xs"
@@ -21,11 +22,33 @@
 
 <script>
 import { ref } from 'vue';
+import { api } from 'boot/axios';
 
 export default {
   setup() {
+    const search = ref('');
+    let timeoutId = null;
+    const SUGGEST_TRIGGER_INTERVAL = 250; // millisencods
+
+    const debouncedSuggest = (newVal) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(async () => {
+        if (newVal) {
+          const query = newVal;
+          try {
+            console.log(`> Get suggestions for query: ${query}`);
+            api.post('/suggest', { query: query }).then((response) => {
+              console.log(response.data);
+            });
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }, SUGGEST_TRIGGER_INTERVAL);
+    };
     return {
-      search: ref(''),
+      search,
+      debouncedSuggest,
     };
   },
 };
