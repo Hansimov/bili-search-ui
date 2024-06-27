d18f5eab4916b19a3109e1dc1c632be2f3aa4d8e
Hansimov <591172499@qq.com>
2024-06-28 07:14:19 +0800

    :recycle: [Refactor] Rename ErrorNotFound to NotFoundPage, and make it under MainLayout

---
+++ quasar/src/pages/NotFoundPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="text-center">
+    <div class="header-placeholder"><br /></div>
+    <div style="font-size: 4vh; opacity: 0.45">…未探索地形…</div>
+    <q-btn
+      class="q-mt-lg"
+      size="4vh"
+      text-color="blue-5"
+      style="opacity: 0.75"
+      unelevated
+      label="回到首页"
+      to="/"
+      no-caps
+    />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'NotFoundPage',
+});
+</script>
+
+<style lang="scss" scoped>
+.header-placeholder {
+  line-height: 12vh;
+  text-align: center;
+}
+</style>
--- quasar/src/router/routes.ts
+++ quasar/src/router/routes.ts
@@ -16,7 +16,8 @@ const routes: RouteRecordRaw[] = [
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/NotFoundPage.vue') }],
   },
 ];
 
