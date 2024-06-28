c95d892d5bc2a28f8cb13d1f4aace42531b7b0dd
Hansimov <591172499@qq.com>
2024-06-29 02:49:54 +0800

    :gem: [Feature] SuggestionItem: highlight suggestion title

--- quasar/src/components/SuggestionItem.vue
+++ quasar/src/components/SuggestionItem.vue
@@ -4,7 +4,7 @@
       <q-icon><img src="../assets/bili-tv.svg" class="tv-icon" /></q-icon>
     </q-item-section>
     <q-item-section class="suggestion-title">
-      {{ suggestion.title }}
+      <div v-html="highlightedTitle"></div>
     </q-item-section>
     <q-item-section side class="suggestion-owner">
       {{ suggestion.owner.name }}
@@ -23,23 +23,39 @@ export default {
       required: true,
     },
   },
+  computed: {
+    highlightedTitle() {
+      if (
+        this.suggestion.common_highlights &&
+        this.suggestion.common_highlights.title
+      ) {
+        return this.suggestion.common_highlights.title[0];
+      } else if (
+        this.suggestion.pinyin_highlights &&
+        this.suggestion.pinyin_highlights['title.pinyin']
+      ) {
+        console.log('highlightedTitle');
+        return this.suggestion.pinyin_highlights['title.pinyin'][0];
+      } else {
+        return this.suggestion.title;
+      }
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .suggestion-avatar,
 .suggestion-owner,
 .suggestion-pubdate {
   flex: 0 0 auto;
 }
-
 .suggestion-title {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   flex-grow: 1;
 }
-
 .suggestion-pubdate {
   text-align: right;
   opacity: 0.65;
