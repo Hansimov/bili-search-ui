eb3da38c74f59872faebd9a3519d24ba9e6d3925
Hansimov <591172499@qq.com>
2024-06-28 19:35:06 +0800

    :zap: [Enhance] SuggestionItem: prettify columns with flex and side, and add owner column

--- quasar/src/components/SuggestionItem.vue
+++ quasar/src/components/SuggestionItem.vue
@@ -1,13 +1,16 @@
 <template>
-  <q-item clickable>
-    <q-item-section avatar class="suggestion-avatar">
+  <q-item clickable :to="`/video/${suggestion.bvid}`">
+    <q-item-section avatar side class="suggestion-avatar">
       <q-icon><img src="../assets/bili-tv.svg" class="tv-icon" /></q-icon>
     </q-item-section>
     <q-item-section class="suggestion-title">
       {{ suggestion.title }}
     </q-item-section>
-    <q-item-section class="suggestion-pubdate">
-      {{ suggestion.pubdate.slice(0, 10) }}
+    <q-item-section side class="suggestion-owner">
+      {{ suggestion.owner.name }}
+    </q-item-section>
+    <q-item-section side class="suggestion-pubdate">
+      {{ suggestion.pubdate_str.slice(0, 10) }}
     </q-item-section>
   </q-item>
 </template>
@@ -24,10 +27,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.suggestion-avatar,
+.suggestion-owner,
+.suggestion-pubdate {
+  flex: 0 0 auto;
+}
+
 .suggestion-title {
   white-space: nowrap;
-  word-wrap: break-word;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  flex-grow: 1;
 }
+
 .suggestion-pubdate {
   text-align: right;
   opacity: 0.65;
