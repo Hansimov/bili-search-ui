9a5fa852ec2e91934c9bc8c63adeecbb0b151847
Hansimov <591172499@qq.com>
2024-06-29 20:04:24 +0800

    :gem: [Feature] New ResultsList and ResultItem components

---
+++ quasar/src/components/ResultItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-card class="result-item" clickable :to="`/video/${result.bvid}`">
+    <q-card-section>
+      <div class="text-h6">{{ result.title }}</div>
+    </q-card-section>
+    <q-card-section>
+      <div class="text-subtitle2">{{ result.owner.name }}</div>
+    </q-card-section>
+    <q-card-section>
+      <div class="text-caption">{{ result.pubdate_str.slice(0, 10) }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    result: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.result-item {
+  padding: 20px;
+}
+</style>
---
+++ quasar/src/components/ResultsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="results-list">
+    <ResultItem
+      v-for="(result, index) in results"
+      :key="index"
+      :result="result"
+    />
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useSearchStore } from '../stores/searchStore';
+import ResultItem from './ResultItem.vue';
+
+export default {
+  components: {
+    ResultItem,
+  },
+  setup() {
+    const searchStore = useSearchStore();
+    const results = computed(() => searchStore.results);
+    return {
+      results,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.results-list {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+}
+</style>
