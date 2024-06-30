2d09bf09e32bc2e5dc95186c55cb64336d935fc0
Hansimov <591172499@qq.com>
2024-06-30 08:08:40 +0800

    :gem: [Feature] ResultItem: load image file with data-proxy-api

--- quasar/src/components/ResultItem.vue
+++ quasar/src/components/ResultItem.vue
@@ -1,18 +1,25 @@
 <template>
   <q-card class="result-item" clickable :to="`/video/${result.bvid}`">
     <q-card-section>
-      <div class="text-h6">{{ result.title }}</div>
+      <div class="text-subtitle1">{{ result.title }}</div>
     </q-card-section>
+    <q-img
+      :src="
+        'https://hansimov-data-proxy-api.hf.space/img?url=' +
+        result.pic +
+        '@320w_200h_1c_!web-space-upload-video.webp'
+      "
+    />
     <q-card-section>
       <div class="text-subtitle2">{{ result.owner.name }}</div>
-    </q-card-section>
-    <q-card-section>
       <div class="text-caption">{{ result.pubdate_str.slice(0, 10) }}</div>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
+import config from '../stores/config.json';
+
 export default {
   props: {
     result: {
@@ -20,11 +27,17 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      coverPicPrefix: config.coverPicPrefix,
+      coverPicSuffix: config.coverPicSuffix,
+    };
+  },
 };
 </script>
 
 <style scoped>
 .result-item {
-  padding: 20px;
+  padding: 10px;
 }
 </style>
