8b8963d524af58741979a88778bbfba2a73a753e
Hansimov <591172499@qq.com>
2024-10-22 14:07:39 +0800

    :zap: [Enhance] ResultsLayout: hide ResultsTypesTab to leave more space for other functions

--- quasar/src/layouts/ResultsLayout.vue
+++ quasar/src/layouts/ResultsLayout.vue
@@ -7,11 +7,11 @@
         <SearchBar />
         <q-space />
       </q-toolbar>
-      <q-toolbar class="q-pb-xs">
+      <!-- <q-toolbar class="q-pb-xs">
         <q-space />
         <ResultsTypesTab />
         <q-space />
-      </q-toolbar>
+      </q-toolbar> -->
     </q-header>
     <q-page-container>
       <router-view />
@@ -22,7 +22,7 @@
 <script setup lang="ts">
 import TitleToolbar from 'components/TitleToolbar.vue';
 import SearchBar from 'src/components/SearchBar.vue';
-import ResultsTypesTab from 'src/components/ResultsTypesTab.vue';
+// import ResultsTypesTab from 'src/components/ResultsTypesTab.vue';
 defineOptions({
   name: 'ResultsLayout',
 });
