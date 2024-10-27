38cdcf3cd502da828acebe1202ccaa514f332daf
Hansimov <591172499@qq.com>
2024-10-27 17:03:23 +0800

    :zap: [Enhance] ai.ts: reuse websockets, and enable update content in aiChatStore

--- quasar/src/functions/ai.ts
+++ quasar/src/functions/ai.ts
@@ -1,11 +1,15 @@
 import { useSearchStore } from '../stores/searchStore';
+import { useAiChatStore } from '../stores/aiChatStore';
 import { Router } from 'vue-router';
+
+const searchStore = useSearchStore();
+const aiChatStore = useAiChatStore();
+const WS_URL = '/ws';
 
 let aiSuggestAbortController = new AbortController();
 const AI_SUGGEST_DEBOUNCE_INTERVAL = 150; // milliseconds
 
 export const aiSuggest = async (newVal: string, showAiSuggestions = true, setSearchStoreAiQuery = true): Promise<void> => {
-    const searchStore = useSearchStore();
     if (setSearchStoreAiQuery) {
         searchStore.setAiQuery(newVal);
     }
@@ -44,12 +48,11 @@ export const aiSuggest = async (newVal: string, showAiSuggestions = true, setSea
     });
 };
 
-const WS_URL = '/ws';
-
-
 export const submitAiQuery = async (aiQueryValue: string, router: Router, isFromURL = false, setSearchStoreAiQuery = true) => {
-    const searchStore = useSearchStore();
-    searchStore.setIsSuggestVisible(false);
+    searchStore.setIsAiSuggestVisible(false);
+    searchStore.setIsAiChatVisible(true);
+    aiChatStore.addMessage('user', aiQueryValue);
+    aiChatStore.addMessage('assistant', '');
     if (aiQueryValue) {
         if (setSearchStoreAiQuery) {
             searchStore.setAiQuery(aiQueryValue);
@@ -62,29 +65,49 @@ export const submitAiQuery = async (aiQueryValue: string, router: Router, isFrom
         }
         try {
             console.log('> submit ai query:', aiQueryValue);
-            const ws = new WebSocket(WS_URL);
-
-            ws.onopen = () => {
-                console.log('+ ws connected');
-                const sent_msg = JSON.stringify({ type: 'chat', info: [{ 'role': 'user', 'content': aiQueryValue }] });
+            const sent_msg = JSON.stringify({ type: 'chat', info: [{ 'role': 'user', 'content': aiQueryValue }] });
+            let ws: WebSocket;
+            if (aiChatStore.ws) {
+                ws = aiChatStore.ws;
+                if (ws.readyState === WebSocket.CLOSED) {
+                    ws = new WebSocket(WS_URL);
+                    aiChatStore.setWebsocket(ws);
+                }
+            } else {
+                ws = new WebSocket(WS_URL);
+                ws.onopen = () => {
+                    console.log('+ ws connected');
+                    ws.send(sent_msg);
+                };
+                ws.onmessage = (event) => {
+                    const response = JSON.parse(event.data);
+                    const msg_type = response.type;
+                    const msg_info = response.info;
+                    if (msg_type === 'chat') {
+                        const role = msg_info.role;
+                        const content = msg_info.content;
+                        if (role === 'stop') {
+                            console.log('x ws stopped');
+                            console.log(aiChatStore.aiChatMessages);
+                        } else {
+                            if (content) {
+                                aiChatStore.appendToActiveMessage(content);
+                            }
+                        }
+                    }
+                };
+                ws.onerror = (error) => {
+                    console.error('x ws error:', error);
+                };
+                ws.onclose = () => {
+                    console.log('* ws closed');
+                };
+                aiChatStore.setWebsocket(ws);
+            }
+            if (ws.readyState === WebSocket.OPEN) {
                 ws.send(sent_msg);
-                console.log(`+ sent ai query: ${aiQueryValue}`);
-            };
-
-            ws.onmessage = (event) => {
-                const response = JSON.parse(event.data);
-                console.log(`recv: [${response.info}]`);
-                // searchStore.setAiSuggestions(response.choices);
-            };
-
-            ws.onerror = (error) => {
-                console.error('x ws error:', error);
-            };
-
-            ws.onclose = () => {
-                console.log('* ws closed');
-            };
-
+            }
+            console.log(`+ sent ai query: ${aiQueryValue}`);
         } catch (error) {
             console.error(error);
         }
