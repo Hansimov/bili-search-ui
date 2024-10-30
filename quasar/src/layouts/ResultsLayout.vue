2309e9ceaa699ca23ab2e62cb76379a917edee2c
Hansimov <591172499@qq.com>
2024-10-30 15:07:11 +0800

    :zap: [Enhance] ResultsLayout: move SearchBar to absolute bottom with q-page-sticky

--- quasar/src/layouts/ResultsLayout.vue
+++ quasar/src/layouts/ResultsLayout.vue
@@ -1,20 +1,15 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header>
       <TitleToolbar />
-      <q-toolbar>
-        <q-space />
-        <SearchBar />
-        <q-space />
-      </q-toolbar>
-      <!-- <q-toolbar class="q-pb-xs">
-        <q-space />
-        <ResultsTypesTab />
-        <q-space />
-      </q-toolbar> -->
     </q-header>
     <q-page-container>
-      <router-view />
+      <q-page class="column no-padding">
+        <router-view />
+        <q-page-sticky position="bottom" class="q-pa-md">
+          <SearchBar />
+        </q-page-sticky>
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
