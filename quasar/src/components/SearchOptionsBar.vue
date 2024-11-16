002f53220214080f995c5304c2810b6a27de2207
Hansimov <591172499@qq.com>
2024-11-17 01:14:12 +0800

    :zap: [Enhance] SearchOptionsBar: rename some labels for better readability, and add styles of auto-check-enable

--- quasar/src/components/SearchOptionsBar.vue
+++ quasar/src/components/SearchOptionsBar.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="ai-search-options-bar q-pb-xs q-pr-xs">
     <q-btn-group flat spread>
-      <q-btn dense label="禁用自动纠错" icon="fa-solid fa-check"
-        ><ButtonTooltip text="禁用自动纠错" hidden
+      <q-btn
+        dense
+        :label="$q.screen.gt.xs ? '自动纠错' : '纠错'"
+        icon="fa-solid fa-check"
+        class="auto-check-enabled"
+        ><ButtonTooltip text="自动纠错" hidden
       /></q-btn>
-      <q-btn dense label="搜索历史" icon="fa-solid fa-list">
+      <q-btn
+        dense
+        :label="$q.screen.gt.xs ? '搜索历史' : '历史'"
+        icon="fa-solid fa-list"
+        v-if="false"
+      >
         <ButtonTooltip text="搜索历史" hidden
       /></q-btn>
-      <q-btn dense label="帮助" icon="fa-solid fa-question"
+      <q-btn dense label="帮助" icon="fa-solid fa-question" v-if="false"
         ><ButtonTooltip text="帮助" hidden
+      /></q-btn>
+      <q-btn dense label="换一换" icon="fa-solid fa-refresh"
+        ><ButtonTooltip text="换一换" hidden
       /></q-btn>
       <q-btn dense label="搜索" icon="fa-regular fa-paper-plane">
         <ButtonTooltip text="搜索" hidden
@@ -27,4 +39,17 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+body.body--light {
+  .auto-check-enabled {
+    color: white !important;
+    background-color: var(--main-bg-color-l);
+  }
+}
+body.body--dark {
+  .auto-check-enabled {
+    color: white !important;
+    background-color: var(--main-bg-color-d);
+  }
+}
+</style>
