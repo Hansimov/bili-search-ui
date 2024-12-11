fcabce7b658f29b013940e4092878fb48662483d
Hansimov <591172499@qq.com>
2024-12-11 17:12:18 +0800

    :zap: [Enhance] SuggestReplace: prettify styles of rewrites: index and original keywords as superscript, better colors and paddings, and better hide-and-show logics

--- quasar/src/components/SuggestReplace.vue
+++ quasar/src/components/SuggestReplace.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="suggest-replace">
-    猜你想搜：<span v-for="(rewriteString, index) in rewritesList" :key="index">
-      <a
-        class="suggest-replace-rewrite-item"
-        href="#"
-        @click.prevent="searchRewritedQuery(rewriteString)"
-        >{{ rewriteString }}
-      </a>
-    </span>
-    ，仍然搜索：<a
-      href="#"
+  <div class="suggest-replace" v-if="rewritesList?.length">
+    <span class="suggest-replace-rewrite"
+      ><span v-for="(rewriteString, index) in rewritesList" :key="index">
+        <a
+          class="suggest-replace-rewrite-text"
+          href="#"
+          @click.prevent="searchRewritedQuery(rewriteString)"
+          >{{ rewriteString }}
+        </a>
+        <span
+          class="suggest-replace-rewrite-index"
+          v-if="rewritesList.length > 1 || !isOriginalInRewrites"
+          >{{ index + 1 }}</span
+        >
+        <span
+          class="suggest-replace-rewrite-sep"
+          v-if="index < rewritesList.length - 1"
+        >
+        </span> </span
+    ></span>
+    <span class="suggest-replace-rewrite-sep"></span>
+    <span
       class="suggest-replace-original"
-      @click.prevent="searchOriginalQuery"
-      >{{ keywordsString }}
-    </a>
+      v-if="!isOriginalInRewrites && keywordsString.trim() !== ''"
+      ><a
+        href="#"
+        class="suggest-replace-original-text"
+        @click.prevent="searchOriginalQuery"
+        >{{ keywordsString }}
+      </a>
+      <span class="suggest-replace-original-index">原文</span>
+    </span>
   </div>
 </template>
 
@@ -28,6 +45,7 @@ export default defineComponent({
     const searchStore = useSearchStore();
     const router = useRouter();
     const query = computed(() => searchStore.query);
+    const suggestResultCache = computed(() => searchStore.suggestResultCache);
     const keywords = computed(
       () => searchStore.suggestResultCache[query.value]?.query_info?.keywords
     );
@@ -38,6 +56,9 @@ export default defineComponent({
     const rewritesList = computed(
       () => searchStore.suggestResultCache[query.value]?.rewrite_info?.list
     );
+    const rewritesTuples = computed(
+      () => searchStore.suggestResultCache[query.value]?.rewrite_info?.tuples
+    );
     const searchOriginalQuery = () => {
       submitQuery(query.value, router, false, true, false);
     };
@@ -46,10 +67,16 @@ export default defineComponent({
       const rewriteQuery = `${rewriteString} ${filtersString}`.trim();
       submitQuery(rewriteQuery, router, false, true, false);
     };
+    const isOriginalInRewrites = computed(
+      () => rewritesList.value?.includes(keywordsString.value) || false
+    );
 
     return {
       keywordsString,
       rewritesList,
+      rewritesTuples,
+      suggestResultCache,
+      isOriginalInRewrites,
       searchOriginalQuery,
       searchRewritedQuery,
     };
@@ -59,17 +86,38 @@ export default defineComponent({
 
 <style scoped>
 .suggest-replace {
-  padding: 8px;
+  padding: 0px 1rem 4px 1rem;
+  line-height: 1.75;
 }
 .suggest-replace a {
   text-decoration: inherit;
 }
-.suggest-replace-rewrite-item {
-  color: greenyellow;
-  border: 1px dashed gray;
-  padding: 2px;
+.suggest-replace-rewrite-text,
+.suggest-replace-original-text {
+  display: inline-block;
+  border: 1px solid gray;
+  padding: 0px 4px;
+  border-radius: 4px;
 }
-.suggest-replace-original {
+.suggest-replace-rewrite-text {
+  color: var(--main-color-d);
+}
+.suggest-replace-rewrite-selected {
+  color: greenyellow;
+}
+.suggest-replace-rewrite-index,
+.suggest-replace-rewrite-count,
+.suggest-replace-original-index {
+  color: gray;
+  vertical-align: super;
+  font-size: smaller;
+  padding: 0px 2px;
+}
+.suggest-replace-rewrite-sep {
+  color: gray;
+  padding: 0px 2px;
+}
+.suggest-replace-original-text {
   color: var(--main-color-d);
 }
 </style>
