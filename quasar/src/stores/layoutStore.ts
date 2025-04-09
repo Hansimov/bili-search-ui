3cd204d5d3fea90703e888ecc43f1f6986ca2556
Hansimov <591172499@qq.com>
2025-04-09 08:00:29 +0800

    :gem: [Feature] New LayoutStore.ts: resize listener, auto update screen and results list stype, and remove RecordsStore

---
+++ quasar/src/stores/layoutStore.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+
+export const useLayoutStore = defineStore('layout', {
+    state: () => ({
+        isSearchRecordsVisible: false,
+        screenWidth: window.innerWidth,
+        searchRecordsListWidth: 300
+    }),
+    actions: {
+        toggleSearchRecordsList() {
+            this.isSearchRecordsVisible = !this.isSearchRecordsVisible
+        },
+        updateScreenWidth() {
+            this.screenWidth = window.innerWidth;
+        },
+        updateDrawerWidth(newWidth: number) {
+            this.searchRecordsListWidth = newWidth;
+        },
+        addWindowResizeListener() {
+            window.addEventListener('resize', this.updateScreenWidth);
+        },
+        removeWindowResizeListener() {
+            window.removeEventListener('resize', this.updateScreenWidth);
+        },
+        availableContentWidth() {
+            if (this.isSearchRecordsVisible && typeof this.searchRecordsListWidth === 'number') {
+                return this.screenWidth - this.searchRecordsListWidth;
+            } else {
+                return this.screenWidth;
+            }
+        },
+        isSmallScreen() {
+            return this.availableContentWidth() < 520;
+        },
+        dynamicResultsListStyle() {
+            return {
+                maxWidth: `${Math.min(this.availableContentWidth(), 1280)}px`,
+            }
+        }
+    },
+});
